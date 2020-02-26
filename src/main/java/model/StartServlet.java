43f2cab1c2085da55ff99f50d222f885e47defb7
Waclaw <SlavaVolga625@yandex.ru>
2020-02-26 20:59:32 +0300

    Initial commit

---
+++ src/main/java/model/StartServlet.java
@@ -0,0 +1,33 @@
+package model;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/StartServlet")
+public class StartServlet extends HttpServlet
+{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        char table[][] = new char[3][3];
+        String player1 = request.getParameter("player1");
+        String player2 = request.getParameter("player2");
+
+        HttpSession hsession = request.getSession();
+        hsession.setAttribute("Play1", player1);
+        hsession.setAttribute("Play2", player2);
+        hsession.setAttribute("session_array", table);
+        hsession.setAttribute("player_name", player1);
+        hsession.setAttribute("flag","1");
+        response.sendRedirect("model/Game.jsp");
+    }
+}
---
+++ web/model/Game.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Slava
+  Date: 26.02.2020
+  Time: 2:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Игра</title>
+</head>
+<body>
+
+</body>
+</html>
