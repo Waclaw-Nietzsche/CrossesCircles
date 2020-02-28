ae78601df9dd444abe4cb20fca72a13e532050c8
Waclaw <SlavaVolga625@yandex.ru>
2020-02-28 22:32:28 +0300

    Alpha

--- src/main/java/model/CrossesCircles.java
+++ src/main/java/model/CrossesCircles.java
@@ -5,7 +5,7 @@ public class CrossesCircles
     public char gametable[][] = new char[3][3];
 
     // Конструктор игрового поля
-    CrossesCircles()
+    public CrossesCircles()
     {
         for (int x = 0; x < 3; x++)
         {
@@ -80,5 +80,157 @@ public class CrossesCircles
         }
     }
 
+    public int[] checkGameTable()
+    {
+        int x, y;
+        int count = 0, d = 1;
+        int line[] = {1 , 2};
+        char symbol;
 
+        for(x = 0; x < 3; x++)
+        {
+            count = 0;
+            for(y = 1; y < 3; y++)
+            {
+                if(gametable[x][0] == gametable[x][y])
+                    count++;
+            }
+            if(count == 2)
+            {
+                symbol = gametable[x][0];
+                if(symbol != ' ')
+                {
+                    if(x == 0)
+                        line[0] = 3;
+                    else if(x == 1)
+                        line[0] = 7;
+                    else
+                        line[0] = 8;
+
+                    if(symbol == 'X')
+                    {
+                        line[1] = 1;
+                    }
+                    else
+                    {
+                        line[1] = 2;
+                    }
+                    return line;
+                }
+            }
+            count = 0;
+            for(y = 1; y < 3; y++)
+            {
+                if(gametable[0][x] == gametable[y][x])
+                    count++;
+            }
+            if(count == 2)
+            {
+                symbol = gametable[0][x];
+                if(x == 0)
+                    line[0] = 4;
+                else if(x==1)
+                    line[0] = 9;
+                else
+                    line[0] = 10;
+
+                if(symbol != ' ')
+                {
+                    if(symbol == 'X')
+                    {
+                        line[1] = 1;
+                    }
+                    else
+                    {
+                        line[1] = 2;
+                    }
+                    return line;
+                }
+            }
+            count = 0;
+            for(y = 1; y < 3; y++)
+            {
+                if(gametable[y][y] == gametable[0][0])
+                    count++;
+            }
+            if(count == 2)
+            {
+                symbol = gametable[0][0];
+                line[0] = 5;
+                if(symbol != ' ')
+                {
+                    if(symbol == 'X')
+                    {
+                        line[1] = 1;
+                        return line;
+                    }
+                    else
+                    {
+                        line[1] = 2;
+                        return line;
+                    }
+                }
+            }
+            count = 0;
+            d = 1;
+            for(y = 1; y < 3; y++)
+            {
+                if(gametable[y][d] == gametable[0][2])
+                    count++;
+                d--;
+            }
+            if(count == 2)
+            {
+                symbol = gametable[1][1];
+                line[0] = 6;
+                if(symbol != ' ')
+                {
+                    line[1] = 2;
+                    return line;
+                }
+            }
+
+        }
+        line[1] = 0;
+        return line;
+    }
+
+    public int move()
+    {
+        if(gametable[1][1]==' ')
+        {
+            gametable[1][1]='0';
+            return 1;
+        }
+        for(int i = 0; i <= 2; )
+        {
+            if(gametable[i][0] == ' ')
+            {
+                gametable[i][0] = '0';
+                return 1;
+            }
+            if(gametable[i][2] == ' ')
+            {
+                gametable[i][2] = '0';
+                return 1;
+            }
+            i += 2;
+        }
+        for(int i=0; i <= 1; i++)
+        {
+            int j = 1;
+            if(gametable[i][j] == ' ')
+            {
+                gametable[i][j] = '0';
+                return 1;
+            }
+            if(gametable[j][i] == ' ')
+            {
+                gametable[j][i] = '0';
+                return 1;
+            }
+            j++;
+        }
+        return 0;
+    }
 }
--- src/main/java/servlets/PlayGameServlet.java
+++ src/main/java/servlets/PlayGameServlet.java
@@ -1,22 +1,123 @@
 package servlets;
 
+import model.CrossesCircles;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/PlayGameServlet")
 public class PlayGameServlet extends HttpServlet
 {
+    CrossesCircles CrsCrc = new CrossesCircles();
+    int z = 0;
+
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-
+        z = 0;
+        HttpSession session = request.getSession();
+        String player_1= (String)session.getAttribute("Play1");
+        CrsCrc.clear();
+        session.setAttribute("player_name", player_1);
+        response.sendRedirect("Play.jsp");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        response.setContentType("text/html");
+        HttpSession session = request.getSession();
+        String player_1 = (String)session.getAttribute("Play1");
+        String player_2 = (String)session.getAttribute("Play2");
+        String req = request.getParameter("a");
+        int play = Integer.parseInt(req);
+        int x = 0, y = 0;
+        switch(play)
+        {
+            case 1: x = 0; y = 0;
+                break;
+            case 2: x = 0; y = 1;
+                break;
+            case 3: x = 0; y = 2;
+                break;
+            case 4: x = 1; y = 0;
+                break;
+            case 5: x = 1; y = 1;
+                break;
+            case 6: x = 1; y = 2;
+                break;
+            case 7: x = 2; y = 0;
+                break;
+            case 8: x = 2; y = 1;
+                break;
+            case 9: x = 2; y = 2;
+                break;
+        }
 
+        z++;
+        if(z % 2 == 0)
+            session.setAttribute("player_name", player_1);
+        else
+            session.setAttribute("player_name", player_2);
+        if(z < 10)
+            play(request, response, x, y);
+    }
+
+    private void play(HttpServletRequest request, HttpServletResponse response, int x, int y) throws IOException
+    {
+        HttpSession session = request.getSession();
+        String player_1 = (String)session.getAttribute("Play1");
+        String player_2 = (String)session.getAttribute("Play2");
+
+        int flag;
+        flag = CrsCrc.add(x, y, z);
+        session.setAttribute("flag",flag);
+        if(flag == 0)
+        {
+            z = z - 1;
+            if(z % 2 == 0)
+                session.setAttribute("player_name", player_1 );
+            else
+                session.setAttribute("player_name", player_2);
+            char[][] table = CrsCrc.gametable;
+            session.setAttribute("session_array", table);
+            response.sendRedirect("jspset/Game.jsp");
+        }
+        else if(z > 4)
+        {
+            int[] table = CrsCrc.checkGameTable();
+            session.setAttribute("line", table[0]);
+            if(table[1] == 1)
+            {
+                response.sendRedirect("WinPL1.jsp");
+            }
+            else if(table[1] == 2)
+            {
+                response.sendRedirect("WinPL2.jsp");
+                z = 10;
+            }
+            else if(z == 9)
+            {
+                response.sendRedirect("DrawPVP.jsp");
+            }
+
+            else
+            {
+                char[][] gametable = CrsCrc.gametable;
+                session.setAttribute("session_array", gametable);
+                response.sendRedirect("jspset/Game.jsp");
+            }
+        }
+        else
+        {
+            char[][] gametable = CrsCrc.gametable;
+            session.setAttribute("session_array",gametable);
+            response.sendRedirect("jspset/Game.jsp");
+
+        }
     }
 }
